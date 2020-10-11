0bacb162816e5150d6543c6722185f2e5ad0e15b
ooeunz <yuns994@gmail.com>
2020-10-11 13:14:40 +0900

    Add problem Add_Two_Numbers

---
+++ src/Add_Two_Numbers/ListNode.java
@@ -0,0 +1,18 @@
+package Add_Two_Numbers;
+
+public class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, Add_Two_Numbers.ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
