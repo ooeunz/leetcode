f0e23bc8812ae76b27a9fefc28eacdcd422abbe9
ooeunz <yuns994@gmail.com>
2020-11-02 08:10:13 +0900

    Solving longuest substring without repeating charcters

--- src/Longest_Substring_Without_Repeating_Characters/Solution.py
+++ src/Longest_Substring_Without_Repeating_Characters/Solution.py
@@ -1,17 +1,28 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        start = end = 0
-        package = set()
+        if len(s) < 2:
+            return len(s)
+        start = 0
+        end = 1
+        pkg = set(list(s[start:end]))
+        mx = len(pkg)
         while end < len(s):
-            if start > end:
-                return len(max(package, key=len))
-            if s[start:end] not in package:
-                package.add(s[start:end])
-                end += 1
-            else:
+            while s[end] in pkg:
+                pkg.remove(s[start])
                 start += 1
-        return len(max(package, key=len))
-
+            if start >= end:
+                pkg.add(s[start])
+                end += 1
+            elif s[end] not in pkg:
+                pkg.add(s[end])
+                mx = max(mx, len(pkg))
+                end += 1
+        return mx
 
 s = Solution()
-print(s.lengthOfLongestSubstring('abcabcbb'))
+print(s.lengthOfLongestSubstring("abcabcbb") == 3)
+print(s.lengthOfLongestSubstring("bbbbb") == 1)
+print(s.lengthOfLongestSubstring("pwwkew") == 3)
+print(s.lengthOfLongestSubstring("") == 0)
+print(s.lengthOfLongestSubstring("aab") == 2)
+
