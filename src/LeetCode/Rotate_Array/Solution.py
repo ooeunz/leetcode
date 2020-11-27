a93cf7bc579e03d9aefa610c2adda9e09a265ec2
ooeunz <yuns994@gmail.com>
2020-11-27 19:42:59 +0900

    Another solving Rotate Array

--- src/LeetCode/Rotate_Array/Solution.py
+++ src/LeetCode/Rotate_Array/Solution.py
@@ -2,12 +2,20 @@ from typing import List
 
 
 class Solution:
+    def reverse(self, nums: list, start: int, end: int):
+        while start < end:
+            nums[start], nums[end] = nums[end], nums[start]
+            start += 1
+            end -= 1
+
     def rotate(self, nums: List[int], k: int) -> None:
-        for i in range(k):
-            previous = nums[-1]
-            for j in range(len(nums)):
-                nums[j], previous = previous, nums[j]
-        # This is nothing to do with the answer.
+        n = len(nums)
+        k %= n
+
+        self.reverse(nums, 0, n - 1)
+        self.reverse(nums, 0, k - 1)
+        self.reverse(nums, k, n - 1)
+        # This is nothing to do with the answer
         print(nums)
 
 
