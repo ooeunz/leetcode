32694c8c8c20a7c8176c3b261599b07aafe11483
ooeunz <yuns994@gmail.com>
2020-12-07 09:43:14 +0900

    Solving House Roubber using dynamic programing

--- src/LeetCode/House_Robber /Solution.py
+++ src/LeetCode/House_Robber /Solution.py
@@ -2,22 +2,22 @@ from typing import List
 
 
 class Solution:
-    def find_rich_house(self, idx: int, val: int, nums: list):
-        if idx >= len(nums):
-            return val
-        current_val = val + nums[idx]
-        return max(
-            self.find_rich_house(idx + 2, current_val, nums),
-            self.find_rich_house(idx + 3, current_val, nums)
-        )
-
     def rob(self, nums: List[int]) -> int:
-        if len(nums) == 1:
+        length = len(nums)
+        if length == 0:
+            return 0
+        if length == 1:
             return nums[0]
-        return max(
-            self.find_rich_house(0, 0, nums),
-            self.find_rich_house(1, 0, nums)
-        )
+        if length == 2:
+            return max(nums)
+
+        dp = [0] * length
+        dp[0] = nums[0]
+        dp[1] = max(nums[0], nums[1])
+
+        for i in range(2, length):
+            dp[i] = max(dp[i - 2] + nums[i], dp[i - 1])
+        return dp[-1]
 
 
 s = Solution()
