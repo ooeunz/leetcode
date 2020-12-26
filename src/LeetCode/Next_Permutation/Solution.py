31035c15a4e2e8fd5bc95fe007839d5f148aceb0
ooeunz <yuns994@gmail.com>
2020-12-26 16:03:35 +0900

    Solving Next Permutation

--- src/LeetCode/Next_Permutation/Solution.py
+++ src/LeetCode/Next_Permutation/Solution.py
@@ -1,36 +1,34 @@
 from typing import List
-from itertools import permutations
 
 
 class Solution:
     def nextPermutation(self, nums: List[int]) -> None:
-        if len(nums) == 1:
+        decrease_section = next_large = len(nums) - 1
+        while decrease_section > 0 and nums[decrease_section - 1] >= nums[decrease_section]:
+            decrease_section -= 1
+        if decrease_section == 0:
+            nums.reverse()
             return
-        permu = sorted(list(set(permutations(nums, len(nums)))))
-        start, end = 0, len(permu)
 
-        def change(nums, per):
-            for i in range(len(nums)):
-                nums[i] = per[i]
+        pivot = decrease_section - 1
+        while nums[pivot] >= nums[next_large]:
+            next_large -= 1
+        nums[pivot], nums[next_large] = nums[next_large], nums[pivot]
 
-        while start < end:
-            mid = (start + end) // 2
-            if permu[mid] == tuple(nums):
-                if mid + 1 >= len(permu):
-                    change(nums, permu[0])
-                else:
-                    change(nums, permu[mid + 1])
-                break
-            elif permu[mid] > tuple(nums):
-                end = mid
-            else:
-                start = mid
+        def reverse_lst(lst: list, start, end):
+            while start < end:
+                lst[start], lst[end] = lst[end], lst[start]
+                start += 1
+                end -= 1
+        reverse_lst(nums, pivot + 1, len(nums) - 1)
+        # This is nothing to do with the answer
+        print(nums)
 
 
 s = Solution()
 s.nextPermutation([1, 2, 3])
-s.nextPermutation([5, 1, 1])
-s.nextPermutation([3, 2, 1])
-s.nextPermutation([1, 1, 5])
-s.nextPermutation([1, 3, 2])
-s.nextPermutation([1])
+# s.nextPermutation([5, 1, 1])
+# s.nextPermutation([3, 2, 1])
+# s.nextPermutation([1, 1, 5])
+# s.nextPermutation([1, 3, 2])
+# s.nextPermutation([1])
