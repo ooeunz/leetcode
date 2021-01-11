901ef8ce5eae7a6636468f70be39ed6db1bef932
ooeunz <yuns994@gmail.com>
2021-01-11 19:46:18 +0900

    Solving Gas Station

--- src/LeetCode/Gas_Station/Solution.py
+++ src/LeetCode/Gas_Station/Solution.py
@@ -3,26 +3,16 @@ from typing import List
 
 class Solution:
     def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
-        if sum(cost) > sum(gas):
-            return -1
+        start = acc = total = 0
         for i in range(len(gas)):
-            if gas[i] >= cost[i]:
-                gas_start = gas[i:] + gas[:i]
-                cost_start = cost[i:] + cost[:i]
-
-                def is_possible(gas_start, cost_start):
-                    tank = 0
-                    for g, c in zip(gas_start, cost_start):
-                        tank += g
-                        tank -= c
-                        if tank < 0:
-                            return False
-                    return True
-                if is_possible(gas_start, cost_start):
-                    return i
-        return -1
+            acc += (gas[i] - cost[i])
+            total += (gas[i] - cost[i])
+            if acc < 0:
+                acc = 0
+                start = i + 1
+        return start if total >= 0 else -1
 
 
 s = Solution()
-print(s.canCompleteCircuit([1, 2, 3, 4, 5], [3, 4, 5, 1, 2]))
-print(s.canCompleteCircuit([2, 3, 4], [3, 4, 3]))
+print(s.canCompleteCircuit([1, 2, 3, 4, 5], [3, 4, 5, 1, 2]) == 3)
+print(s.canCompleteCircuit([2, 3, 4], [3, 4, 3]) == -1)
