bc0a6735d806b2c2e44d77f20568808ba7db0f51
ooeunz <yuns994@gmail.com>
2021-03-02 18:16:11 +0900

    Add java code in Median of Two Sorted Arrays

--- src/LeetCode/Median_of_Two_Sorted_Arrays/README.md
+++ src/LeetCode/Median_of_Two_Sorted_Arrays/README.md
@@ -6,6 +6,43 @@
 
 ### solve 1.
 문제에서 **정렬이 된 문자열 2개**를 주기 때문에 merge sort의 마지막 단계, 즉 정렬이 된 두 문자열을 합치는 부분만 구현 함으로써 문제를 풀 수 있습니다.
+
+> java
+```java
+class Solution {
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        int nm1 = 0;
+        int nm2 = 0;
+
+        List<Integer> ans = new ArrayList<>();
+        while (nm1 < nums1.length && nm2 < nums2.length) {
+            if (nums1[nm1] < nums2[nm2]) {
+                ans.add(nums1[nm1++]);
+            } else {
+                ans.add(nums2[nm2++]);
+            }
+        }
+
+        if (nm1 < nums1.length) {
+            while (nm1 < nums1.length) {
+                ans.add(nums1[nm1++]);
+            }
+        } else {
+            while (nm2 < nums2.length) {
+                ans.add(nums2[nm2++]);
+            }
+        }
+
+        int mid = ans.size() / 2;
+        if (ans.size() % 2 == 0) {
+            return (float) (ans.get(mid - 1) + ans.get(mid)) / 2;
+        }
+        return ans.get(mid);
+    }
+}
+```
+
+> python
 ```python
 from typing import List
 import collections
---
+++ src/LeetCode/Median_of_Two_Sorted_Arrays/Solution.java
@@ -0,0 +1,48 @@
+package LeetCode.Median_of_Two_Sorted_Arrays;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        int nm1 = 0;
+        int nm2 = 0;
+
+        List<Integer> ans = new ArrayList<>();
+        while (nm1 < nums1.length && nm2 < nums2.length) {
+            if (nums1[nm1] < nums2[nm2]) {
+                ans.add(nums1[nm1++]);
+            } else {
+                ans.add(nums2[nm2++]);
+            }
+        }
+
+        if (nm1 < nums1.length) {
+            while (nm1 < nums1.length) {
+                ans.add(nums1[nm1++]);
+            }
+        } else {
+            while (nm2 < nums2.length) {
+                ans.add(nums2[nm2++]);
+            }
+        }
+
+        int mid = ans.size() / 2;
+        if (ans.size() % 2 == 0) {
+            return (float) (ans.get(mid - 1) + ans.get(mid)) / 2;
+        }
+        return ans.get(mid);
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+
+        int[] nums1 = {1, 3};
+        int[] nums2 = {2};
+        System.out.println(solution.findMedianSortedArrays(nums1, nums2) == 2.0);
+
+        nums1 = new int[]{1, 2};
+        nums2 = new int[]{3, 4};
+        System.out.println(solution.findMedianSortedArrays(nums1, nums2) == 2.5);
+    }
+}
