237ec5900bfb5b6cf64d89a3bec2060807b64646
ooeunz <yuns994@gmail.com>
2021-03-07 16:06:18 +0900

    Solving Flatten Binary Tree to Linked List

---
+++ src/LeetCode/Flatten_Binary_Tree_to_Linked_List/Solution.java
@@ -0,0 +1,32 @@
+package LeetCode.Flatten_Binary_Tree_to_Linked_List;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution {
+    public void flatten(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        List<Integer> arr = new ArrayList<>();
+        dfs(arr, root);
+        for (int i = 1; i < arr.size(); i++) {
+            int val = arr.get(i);
+            root.right = new TreeNode(val);
+            root = root.right;
+        }
+    }
+
+    public void dfs(List<Integer> arr, TreeNode node) {
+        arr.add(node.val);
+
+        if (node.left != null) {
+            dfs(arr, node.left);
+            node.left = null;
+        }
+        if (node.right != null) {
+            dfs(arr, node.right);
+            node.right = null;
+        }
+    }
+}
---
+++ src/LeetCode/Flatten_Binary_Tree_to_Linked_List/TreeNode.java
@@ -0,0 +1,14 @@
+package LeetCode.Flatten_Binary_Tree_to_Linked_List;
+
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
